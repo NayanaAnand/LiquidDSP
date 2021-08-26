de86b1acc861c425a466e8cfe198fb545ee880bf
Nayana N R <nayana.nr@cyient.com>
2021-08-26 10:37:29 +0530

    Opensource LiquidDSP

---
+++ cpp/include/LiquidDSP.h
@@ -0,0 +1,8 @@
+#ifndef LIQUIDDSP_H
+#define LIQUIDDSP_H
+
+/*
+ * TODO: Declare library classes and functions here
+ */
+
+#endif // LIQUIDDSP_H
---
+++ cpp/src/LiquidDSP.cpp
@@ -0,0 +1,5 @@
+#include "LiquidDSP.h"
+
+/*
+ * TODO: Implement library classes and functions here
+ */
